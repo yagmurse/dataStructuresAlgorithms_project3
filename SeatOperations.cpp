2fef2118e4405060a4e7e0f5bc51048006b5cef5
Çağrı <gokceca@itu.edu.tr>
2019-11-10 10:26:30 +0300

    Initial commit

---
+++ SeatOperations.cpp
@@ -0,0 +1,25 @@
+/*
+Student Name:
+Student Number:
+Project Number: 3
+Compile Status: [SUCCESS/FAIL]
+Running Status: [SUCCESS/FAIL]
+Notes: Anything you want to say about your code that will be helpful in the grading process.
+*/
+#include "SeatOperations.h"
+
+using namespace std;
+
+SeatOperations::SeatOperations(int N, int M){
+    // IMPLEMENT ME!
+}
+
+void SeatOperations::addNewPerson(int personType, const string& ticketInfo){
+    // IMPLEMENT ME!
+}
+
+void SeatOperations::printAllSeats(ofstream& outFile){
+    // IMPLEMENT ME!
+}
+
+// YOU CAN ADD YOUR HELPER FUNCTIONS
